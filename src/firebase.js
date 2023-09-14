efa7257af61c6ef27058384b9ea1c1145b1f9810
prat-kap <kapoorprateek91@gmail.com>
2023-09-15 03:40:20 +0530

    removed unused file

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,6 @@ import { getFirestore } from "firebase/firestore"
 import "firebase/auth"
 
 const firebaseApp = initializeApp({
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   apiKey: "AIzaSyDw9VRDizpkt3IGGKalheZ5FSl0In9Yqlo",
   authDomain: "task-management-app-d8411.firebaseapp.com",
   databaseURL: "https://task-management-app-d8411-default-rtdb.firebaseio.com",
